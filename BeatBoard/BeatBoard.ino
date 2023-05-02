03d2175b842e3461ef8051bffbdfbd8a634105b3
Ashir Imran <71806327+xlinkfrostx@users.noreply.github.com>
2023-05-02 14:04:54 -0400

    Testing 1 button and 1 buzzer

--- BeatBoard/BeatBoard.ino
+++ BeatBoard/BeatBoard.ino
@@ -1,9 +1,29 @@
+#define BUTTON 13
+#define BUZZER 15
+
 void setup() {
   // put your setup code here, to run once:
-
+  pinMode(BUTTON, INPUT_PULLUP);
+  pinMode(BUZZER, OUTPUT);
+  Serial.begin(9600);
 }
+
+unsigned long debounceTime = 200;
+unsigned long lastDebounceTime = 0;
 
 void loop() {
   // put your main code here, to run repeatedly:
+  if(millis() - lastDebounceTime > debounceTime && digitalRead(BUTTON) == 0) {
+      // Serial.println("HERE");
+      tone(BUZZER, 698, 500);
+      // digitalWrite(BUZZER, HIGH);
+      lastDebounceTime = millis();
+    }
 
+    // if(digitalRead(BUTTON) == LOW) {
+    //   digitalWrite(BUZZER, LOW);
+    // }
+  
+
+  // noTone(BUZZER);
 }
