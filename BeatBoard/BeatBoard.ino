6fcef19c2b8e94c839af1043514df1aeeae0e127
Ashir Imran <71806327+xlinkfrostx@users.noreply.github.com>
2023-05-04 15:21:47 -0400

    added led

--- BeatBoard/BeatBoard.ino
+++ BeatBoard/BeatBoard.ino
@@ -1,36 +1,28 @@
 #define BUTTON_ONE 13
-#define BUTTON_TWO 14
+#define LED 12
 #define BUZZER_ONE 15
-#define BUZZER_TWO 12
 
 void setup() {
-  // put your setup code here, to run once:
-  pinMode(BUTTON_ONE, INPUT_PULLUP);
-  pinMode(BUTTON_TWO, INPUT_PULLUP);
-  pinMode(BUZZER_ONE, OUTPUT);
-  pinMode(BUZZER_TWO, OUTPUT);
-
   Serial.begin(9600);
+  // initialize the piezo as output:
+  pinMode(BUZZER_ONE, OUTPUT);  
+  // initialize the pushbutton pin as an input:
+  pinMode(BUTTON_ONE, INPUT_PULLUP);
+  // initialize the led pin as an output:
+  pinMode(LED, OUTPUT);
 }
-
 unsigned long debounceTime = 200;
 
 unsigned long lastDebounceTime = 0;
 unsigned long lastDebounceTime2 = 0;
-
-void loop() {
-  // put your main code here, to run repeatedly:
-  if(millis() - lastDebounceTime > debounceTime && digitalRead(BUTTON_ONE) == 0) {
-    tone(BUZZER_ONE, 698);
-    lastDebounceTime = millis();
-  } else if (digitalRead(BUTTON_ONE) == 1) {
+void loop(){
+  // read the state of the pushbutton value:
+  if(millis() - lastDebounceTime > debounceTime && digitalRead(BUTTON_ONE) == 1) {
+    digitalWrite(LED, HIGH);
+    tone(BUZZER_ONE,400,200);
+    delay(500);
+  } else if (digitalRead(BUTTON_ONE) == 0) {
+    digitalWrite(LED, LOW);
     tone(BUZZER_ONE, 0);
   }
-
-  // if(millis() - lastDebounceTime2 > debounceTime && digitalRead(BUTTON_TWO) == 0) {
-  //   tone(BUZZER_TWO, 1975);
-  //   lastDebounceTime2 = millis();
-  // } else if (digitalRead(BUTTON_TWO) == 1) {
-  //   tone(BUZZER_TWO, 0);
-  // }
 }
